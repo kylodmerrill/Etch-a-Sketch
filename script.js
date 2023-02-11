fcfeb290477bf75a11c5db054f5112dabead54bc
Kyle Merrill <kylemerrill18@gmail.com>
2023-02-11 12:44:30 -0700

    Change pixel color on hover permanently rather than temporarily

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ for (let i = 0; i < 256; i++) {
     box.className = "box";
 
     document.getElementById('grid').appendChild(box);
-}
+
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "aqua";
+    }
+)}
 
 const btn = document.querySelector('button');
 const grid = document.getElementById('grid');
@@ -21,13 +25,17 @@ btn.addEventListener('click', () => {
     const dim = prompt("Please enter a number between 1 and 100", "16");
     const dimSqrd = Math.pow(dim, 2);
 
-    // generate new grid
+    // generate new grid and change box color on hover
     for (let i = 0; i < dimSqrd; i++) {
     const box = document.createElement('div');
     box.className = "box";
 
     document.getElementById('grid').appendChild(box);
+
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "aqua";
     }
+    )}
 
     // adjust box sizing
     grid.style.gridTemplateColumns = `repeat(${dim}, 1fr)`;
